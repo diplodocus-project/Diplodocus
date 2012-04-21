232b97a3ac28f75aed5847f24a0e2560cd1f6ae7
Theo crevon <oleiade@new-host.home>
2012-04-21 23:37:54 +0200

    Add : Base script for french map visualisation generation:

---
+++ scripts/visualisations/france_map.py
@@ -0,0 +1,23 @@
+import kartograph
+
+opts = {
+    "proj": {
+        "id": "ortho",
+        "lon0": 3.0,
+        "lat0": 48.0,
+        },
+    "bounds": {
+        "mode": "bbox",
+        "data": [-100, 30, -60, 60]
+        },
+    "layers": [{
+        "src": "shp/ne_50m_admin_0_countries.shp",
+        }],
+    "export": {
+        "width": 800,
+        "height": "auto",
+        },
+    }
+
+k = kartograph.Kartograph()
+k.generate(opts, outfile='map.svg')
